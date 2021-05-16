dbeb9774ed260ad2fac2c3b1ca79823f94b1bb7b
softwareeducation-net <47324217+softwareeducation-net@users.noreply.github.com>
2021-05-16 08:37:25 -0400

    CSS updates

--- src/pages/TradePage.tsx
+++ src/pages/TradePage.tsx
@@ -176,13 +176,13 @@ function TradePageInner() {
                 title="Market address"
                 trigger="click"
               >
-                <InfoCircleOutlined style={{ color: '#2abdd2' }} />
+                <InfoCircleOutlined style={{ color: '#00FEBC' }} />
               </Popover>
             </Col>
           ) : null}
           <Col>
             <PlusCircleOutlined
-              style={{ color: '#2abdd2' }}
+              style={{ color: '#00FEBC' }}
               onClick={() => setAddMarketVisible(true)}
             />
           </Col>
