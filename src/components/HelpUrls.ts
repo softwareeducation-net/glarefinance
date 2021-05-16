010b4cde774631893c080d1533aca0be5ae90a88
softwareeducation-net <47324217+softwareeducation-net@users.noreply.github.com>
2021-05-16 08:07:08 -0400

    Referal Updates

--- src/components/HelpUrls.ts
+++ src/components/HelpUrls.ts
@@ -1,11 +1,8 @@
 export const helpUrls = {
-  customerSupport: 'https://t.me/ProjectSerum',
-  customerSupportZh: 'https://t.me/ProjectSerum_Chinese',
-  contactEmail: 'mailto:contact@projectserum.com',
-  discord: 'https://discord.gg/EDvudv6',
-  telegram: 'https://t.me/ProjectSerum',
-  github: 'https://github.com/project-serum',
+  contactEmail: 'mailto:contact@glare.finance',
+  discord:  'https://discord.gg/Dw42VXSdDY',
+  telegram: 'https://t.me/glarefinance',
+  twitter: 'https://twitter.com/FinanceGlare',
   projectSerum: 'https://projectserum.com/',
-  developerResources: 'https://projectserum.com/developer-resources',
   solanaBeach: 'https://solanabeach.io',
 };
