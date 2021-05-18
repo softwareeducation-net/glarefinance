839836e3cf31862287d23eabf2b0fbaacd0168d6
softwareeducation-net <47324217+softwareeducation-net@users.noreply.github.com>
2021-05-18 18:11:59 -0400

    revert to makret landing page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-        import Link from '../components/Link';
-        import { Row, Col } from 'antd';
 
 
         export default function home() {
+
         return (
-        <Row justify="center">
-        <Col>
-                 <Link external to="google.com">
-                "Enter App"
-                </Link>
-        </Col>
-        </Row>
+                <h1>Hello</h1>
+
+
 
 
         );
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,6 @@
 import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 import TradePage from './pages/TradePage';
 import OpenOrdersPage from './pages/OpenOrdersPage';
-        import home from './pages/home';
-
 import React from 'react';
 import BalancesPage from './pages/BalancesPage';
 import ConvertPage from './pages/ConvertPage';
@@ -11,7 +9,7 @@ import ListNewMarketPage from './pages/ListNewMarketPage';
 import NewPoolPage from './pages/pools/NewPoolPage';
 import PoolPage from './pages/pools/PoolPage';
 import PoolListPage from './pages/pools/PoolListPage';
-//import { getTradePageUrl } from './utils/markets';
+import { getTradePageUrl } from './utils/markets';
 
 export function Routes() {
   return (
@@ -20,7 +18,7 @@ export function Routes() {
         <BasicLayout>
           <Switch>
             <Route exact path="/">
-              <Redirect to="/home" component={home}  />
+              <Redirect to={getTradePageUrl()} />
             </Route>
             <Route exact path="/market/:marketAddress">
               <TradePage />
